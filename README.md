b634deb892b7d34c51c5e5f070f9810cd48cee05
amitkrishna <git@stackblitz.com>
2024-11-10 14:31:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-coding-Challenges
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/amitkrishna/JS-coding-Challenges)
---
+++ index.js
@@ -0,0 +1,14 @@
+// Example input : "Hello"
+// Example output: "HELLO!"
+
+//Example input: "I'm almost out of coffee"
+//Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+
+function panic(str) {
+  return str.split(' ').join(' 😱 ').toUpperCase() + '!';
+}
+
+// Test your function
+console.log(panic("I'm almost out of coffee"));
+console.log(panic('winter is coming'));
+console.log(panic('Hello'));
